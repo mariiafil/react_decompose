63eb881815a022b8993acd7c499c12f97e672985
masha <mariafil2107@gmail.com>
2020-02-11 13:40:31 +0200

    Updates

---
+++ precache-manifest.92d3e5eb0347da068886a8181db8c5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fda1a665cdc361db3769ceb047852f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12f5900f66eb7b1d7d5e",
+    "url": "./static/css/main.853d2f2e.chunk.css"
+  },
+  {
+    "revision": "7c6debf4786038411135",
+    "url": "./static/js/2.4e5947cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.4e5947cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12f5900f66eb7b1d7d5e",
+    "url": "./static/js/main.31e4f219.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_decompose/precache-manifest.76c341ed9ab3d1937566b756824c6be2.js"
+  "./precache-manifest.92d3e5eb0347da068886a8181db8c5c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_decompose/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
